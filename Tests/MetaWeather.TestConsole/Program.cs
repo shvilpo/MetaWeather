282209aff992cdcb555c887153d26932a44b95b5
shvilpo <shvilpo@gmail.com>
2022-06-17 14:54:48 +0300

    (ADD) App's servises

--- Tests/MetaWeather.TestConsole/Program.cs
+++ Tests/MetaWeather.TestConsole/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MetaWeather.TestConsole
+{
+    class Program
+    {
+        private static IHost __Hosting;
+        public static IHost Hosting => __Hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+        public static IServiceProvider Services => Hosting.Services;
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(ConfigureServices);
+        private static void ConfigureServices(HostBuilderContext arg1, IServiceCollection arg2)
+        { 
+        
+        }
+
+        static async Task Main(string[] args)
+        {
+            using IHost? host = Hosting;
+            await host.StartAsync();
+
+            Console.WriteLine("Завершено");
+            Console.ReadLine();
+
+            await host.StopAsync();
+        }
+    }
+}
